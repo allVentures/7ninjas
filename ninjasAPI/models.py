60d5c69a331bcaf187901e1c56e1e716c8e520b1
allVentures <paul@allphotobangkok.com>
2019-01-23 18:12:04 +0100

    models and fake DB data

--- ninjasAPI/models.py
+++ ninjasAPI/models.py
@@ -1,3 +1,49 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils.timezone import now
 
-# Create your models here.
+CURRENCY = (
+    (1, "PLN"),
+    (2, "EUR"),
+    (3, "USD"),
+    (4, "GBP"),
+)
+
+# You probably need some more information about customers and extension of the User model (address etc.) , I'm gonna skip that as it wasn't
+#specified in the excercise description
+
+class Currency_Rates(models.Model):
+    currency = models.IntegerField(choices=CURRENCY, default=1)
+    currency_rate = models.FloatField()
+
+
+class ProductCategory(models.Model):
+    product_category = models.CharField(max_length=64, unique=True)
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField(max_length=512, null=True)
+    price = models.FloatField()
+    currency = models.IntegerField(choices=CURRENCY, default=1)
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE, verbose_name="product_category")
+    picture = models.ImageField(null=True)
+
+
+class Delivery(models.Model):
+    title = models.CharField(max_length=128)
+    fixed_price_delivery = models.FloatField(null=True)
+    percentage_delivery_price = models.FloatField(null=True)
+
+# for simplification - SalesOrder.id to sales order number
+class SalesOrder(models.Model):
+    customer = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="customer")
+    delivery = models.ForeignKey(Delivery, on_delete=models.CASCADE, verbose_name="delivery")
+    order_date = models.DateTimeField(editable=False, default=now)
+
+
+class SalesOrderItem(models.Model):
+    sales_order_number = models.ForeignKey(SalesOrder, on_delete=models.CASCADE, verbose_name="order_number")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name="product")
+    quantity = models.IntegerField()
+
---
+++ working_scripts.py
@@ -0,0 +1,83 @@
+import random
+from django.contrib.auth.models import User
+from faker import Faker
+from ninjasAPI.models import ProductCategory, Product, Delivery, SalesOrder, SalesOrderItem
+
+fake = Faker('pl_PL')
+
+# Generate 20 fake users
+username_base = "user"
+
+for x in range(1, 21):
+    email = fake.email()
+    first_name = fake.first_name()
+    last_name = fake.last_name()
+    User.objects.create_user(
+        username=username_base + str(x),
+        password="1234",
+        first_name=first_name,
+        last_name=last_name,
+        email=email,
+    )
+
+# genrate 10 random categories
+for x in range(1, 11):
+    cat_obj = ProductCategory()
+    words = fake.words(nb=1, ext_word_list=None, unique=True)
+    category = "Kategoria " + words[0]
+    cat_obj.product_category = category
+    cat_obj.save()
+
+
+# Generate random products (10 in each category)
+
+all_categories = ProductCategory.objects.all()
+for cat in all_categories:
+    for x in range(1, 11):
+        product = Product()
+        words = fake.words(nb=2, ext_word_list=None, unique=True)
+        product.title = words[0] + " " + words[1]
+        product.description = fake.sentence(nb_words=30, variable_nb_words=True, ext_word_list=None)
+        product.price = random.randint(1, 100) + (random.randint(1, 100)/100)
+        product.currency = random.randint(1, 4)
+        product.category_id = cat.id
+        product.picture = "prod_pic_" + str(cat.id) + "_" + str(x) + ".jpg"
+        product.save()
+
+# Generate random delivery options, 5 fixed and 5 %-based
+
+for x in range(1, 6):
+    delivery = Delivery()
+    delivery.title = fake.sentence(nb_words=5, variable_nb_words=True, ext_word_list=None)
+    delivery.fixed_price_delivery = random.randint(1, 20) + (random.randint(1, 100)/100)
+    delivery.save()
+
+for x in range(1, 6):
+    delivery = Delivery()
+    delivery.title = fake.sentence(nb_words=5, variable_nb_words=True, ext_word_list=None)
+    delivery.percentage_delivery_price = random.randint(1, 5)/100
+    delivery.save()
+
+
+# random sales orders (100)
+for x in range(1,101):
+    order = SalesOrder()
+    random_user = random.randint(1, 20)
+    random_delivery_method = random.randint(1, 10)
+    order.customer_id = random_user
+    order.delivery_id = random_delivery_method
+    order.save()
+
+# populate 5 order items for each order
+orders = SalesOrder.objects.all()
+for ord in orders:
+    for x in range(1,6):
+        order_item = SalesOrderItem()
+        random_product = random.randint(1, 100)
+        random_quantity = random.randint(1, 30)
+        order_item.quantity = random_quantity
+        order_item.product_id = random_product
+        order_item.sales_order_number_id = ord.id
+        order_item.save()
+
+# python3 manage.py shell < working_scripts.py
